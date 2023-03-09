4d468a824db52826c46d1cf145c7233f5247cd2f
Dom <111844962+dom-io@users.noreply.github.com>
2023-03-09 12:58:00 -0800

    Add project files.

---
+++ BrowserGame/Client/Pages/Counter.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BrowserGame.Client.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+    private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
+
